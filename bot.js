cd9c336e0e7e73e92fcd5b5d1f7b09e188e02e6c
unknown <ianmosk19@gmail.com>
2019-08-19 23:17:08 -0400

    Axios testing

--- bot.js
+++ bot.js
@@ -17,20 +17,17 @@ client.on('message' , message => {
     {
         // testing - trying to scrape the requisite lolchess pages
         // really inefficient but working as is for now
-        axios.all([axios.get('https://lolchess.gg/profile/na/iceman0160') ,
-                   axios.get('https://lolchess.gg/profile/na/sanuksom')
-                 ]).then(axios.spread((res1 , res2) => {
+        let res1 , res2 = axios.all([axios.get('https://lolchess.gg/profile/na/iceman0160') ,
+                                     axios.get('https://lolchess.gg/profile/na/sanuksom')])
 
-                     // could loop this or something , no repeating needed
-                     cheer1 = cheerio.load(res1.data)
-                     user1 = cheer1('span.profile__summoner__name')
-                     console.log(cheer1(user1).text())
+        // could loop this or something , no repeating needed
+        cheer1 = cheerio.load(res1.data)
+        user1 = cheer1('span.profile__summoner__name')
+        console.log(cheer1(user1).text())
 
-                     cheer2 = cheerio.load(res2.data)
-                     user2 = cheer2('span.profile__summoner__name')
-                     console.log(cheer2(user2).text())
-
-        }))
+        cheer2 = cheerio.load(res2.data)
+        user2 = cheer2('span.profile__summoner__name')
+        console.log(cheer2(user2).text())
     }
 
 })
