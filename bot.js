dbb6de99b2e20b4e13d7be4c6d6895436a0b9963
unknown <ianmosk19@gmail.com>
2019-08-19 23:05:14 -0400

    Starting testing scraping lolchess...

--- bot.js
+++ bot.js
@@ -1,23 +1,39 @@
-const Discord = require('discord.js') ;
-const client = new Discord.Client() ;
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const cheerio = require('cheerio')
+const axios = require('axios')
 
 client.on('ready' , () => {
 
-  console.log('I am ready!') ;
+    console.log('I am ready!')
 
-}) ;
+})
 
 
 client.on('message' , message => {
 
-  // message checking
-  if (message.content === 'ping')
-  {
-    // what pinging this message type will do
-    message.reply('pong')
-  }
+    // message checking
+    if (message.content === '!ranks')
+    {
+        // testing - trying to scrape the requisite lolchess pages
+        // really inefficient but working as is for now
+        axios.all([axios.get('https://lolchess.gg/profile/na/iceman0160') ,
+                   axios.get('https://lolchess.gg/profile/na/sanuksom')
+                 ]).then(axios.spread((res1 , res2) => {
 
-}) ;
+                     // could loop this or something , no repeating needed
+                     cheer1 = cheerio.load(res1.data)
+                     user1 = cheer1('span.profile__summoner__name')
+                     console.log(cheer1(user1).text())
+
+                     cheer2 = cheerio.load(res2.data)
+                     user2 = cheer2('span.profile__summoner__name')
+                     console.log(cheer2(user2).text())
+
+        }))
+    }
+
+})
 
 
 client.login(process.env.BOT_TOKEN) ;
