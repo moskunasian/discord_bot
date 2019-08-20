48ffecb3e737964814c993b5a32b00595843dc7a
Moskunas <ianmosk19@gmail.com>
2019-08-20 15:06:59 -0400

    Confirming working on prod

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const client = new Discord.Client()
 const cheerio = require('cheerio')
 const axios = require('axios')
 
+
 client.on('ready' , () => {
 
     console.log('I am ready!')
@@ -10,15 +11,48 @@ client.on('ready' , () => {
 }) ;
 
 
-client.on('message' , message => {
+client.on('message' , message =>
+{
 
-    if (message.content === '!ranks')
+    function doneSort(outputList)
     {
-        message.reply('!ranks , not implemented yet. . .')
+         // this function will do the sorting ?
+         // sort based on matching groups from other array - type situation
+         message.reply('testing before ranking: ')
+         message.reply(outputList)
     }
 
 
+    // 'sort subset of list?'
+    // tiers = ["Iron" , "Bronze" , "Silver" , "Gold" , "Platinum" , "Diamond"]
+    // divisions = ["4" , "3" , "2" , "1"]
+    // individiual LP values
+    let currNames = ["iceman0160" , "sanuksom" , "Rexox" , "Pentameme" ,
+                     'Swash' , 'K0BI' , 'Hisoka x Chrollo' , 'VioIation']
+    let output = []
+    let count = 0
+
+    if (message.content === '!ranks')
+    {
+        for (let i = 0 ; i < currNames.length ; i++)
+        {
+            axios.get('https://lolchess.gg/profile/na/' + currNames[i]).then(function(result)
+            {
+                $ = cheerio.load(result.data)
+                userTier = $('span.profile__tier__summary__tier')
+                userDiv = $(userTier).text().slice(-1)
+                userTier = $(userTier).text().slice(0 , -1)
+                userName = $('span.profile__summoner__name')
+                userLP = $('span.profile__tier__summary__lp')
+                output.push([$(userName).text() , userTier , userDiv , $(userLP).text().slice(0 , 2)])
+                count++
+
+                if (count == currNames.length)
+                    doneSort(output)
+            })
+        }
+    }
 }) ;
 
 
-client.login(process.env.BOT_TOKEN) ;
+client.login(process.env.BOT_TOKEN)
