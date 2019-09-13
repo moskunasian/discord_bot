30a8837bc0d8ea15835e0840a97a2d45175da773
Moskunas <ianmosk19@gmail.com>
2019-09-13 14:11:38 -0400

    New command added, cleaning up, using cache for speedups, etc.

--- bot.js
+++ bot.js
@@ -1,85 +1,86 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
-const cheerio = require('cheerio')
-const axios = require('axios')
+// Ian Moskunas / ianmosk19@gmail.com
 
-// to do:   better method structure and modularity
-//          better string formatting so resembles normal convention
-//          cleaner column formatting in leaderboard output
-//          add bot description / activity for better appearence on server
+const Discord = require("discord.js") ;
+const client = new Discord.Client() ;
+const cheerio = require("cheerio") ;
+const axios = require("axios") ;
 
-client.on('ready' , () => {
 
-    console.log('I am ready!')
-    client.user.setPresence(({name: 'Just TFT Tings...' , type: 0}))
+client.on("ready" , () => {
+
+    console.log("I am ready!") ;
+    client.user.setPresence(({name: "Just TFT Tings..." , type: 0})) ;
 
 }) ;
 
 
-client.on('message' , message =>
+// have !update command to refresh what is present in the output list
+// then have ranks , which will just output whatever was last in update command
+client.on("message" , message =>
 {
-    function doneSort(outputList)
+    function rankSortOutput(outputList)
     {
-         // 0           1       2           3
-         // userName    tier    division    lp
-         var map = {}
-         tiers = ["Iron" , "Bronze" , "Silver" , "Gold" , "Platinum" , "Diamond"]
-         for (let i = 0 ; i < tiers.length ; i++)
-            map[tiers[i]] = i
+        var rankMap = {} ;
+        rankTiers = ["Iron" , "Bronze" , "Silver" , "Gold" , "Platinum" , "Diamond"] ;
+        for (let i = 0 ; i < rankTiers.length ; i++)
+            rankMap[rankTiers[i]] ;
 
-        // sorts by tier correctly
         outputList.sort(function(a , b)
         {
-            return (map[a[1]] - map[b[1]]) || (b[2] - a[2]) || (a[3] - b[3])
+            return (rankMap[a[1]] - rankMap[b[1]]) || (b[2] - a[2]) || (a[3] - b[3]) ;
         })
-        outputList.reverse()
+        outputList.reverse() ;
 
-        // outputting testing
-        let outputString = ''
-        let maxName = 0
+        var maxName = 0 ;
         for (let i = 0 ; i < outputList.length ; i++)
         {
             if (outputList[i][0].length > maxName)
-                maxName = outputList[i][0].length
+                maxName = outputList[i][0].length ;
         }
 
+        var outputString = "" ;
         for (let i = 0 ; i < outputList.length ; i++)
         {
-            currLen = outputList[i][0].length
-            outputString += '\n' + (i + 1) + '. ' +
-                            outputList[i][0] + ': ' +
-                            outputList[i][1] + ' ' +
-                            outputList[i][2] + ' ' +
-                            outputList[i][3] + 'LP'
+            currLen = outputList[i][0].length ;
+            outputString += "\n" + (i + 1) + ". " +
+                            outputList[i][0] + ": " +
+                            outputList[i][1] + " " +
+                            outputList[i][2] + " " +
+                            outputList[i][3] + "LP" ;
         }
-        message.reply('\nCurrent TFT Leaderboard:\n---' + outputString + '\n---')
+        message.reply("\nCurrent TFT Leaderboard:\n---" + outputString + "\n---") ;
     }
 
-    let currNames = ["Snakebite0160" , "sanuksom" , "Rexox" , "Pentameme" ,
-                     'Swash' , 'K0BI' , 'Hisoka x Chrollo' , 'VioIation' , 'FattyTerps']
-    let output = []
-    let count = 0
+    let rankCache = []
 
-    if (message.content === '!ranks')
+    if (message.content == "!update")
     {
+        var count = 0 ;
+        var currNames = ["Snakebite0160" , "sanuksom" , "Rexox" , "Pentameme" ,
+                         "Swash" , "K0BI" , "Hisoka x Chrollo" , "VioIation" , "FattyTerps"] ;
+
         for (let i = 0 ; i < currNames.length ; i++)
         {
-            // this is slow because GET'ing each username , realtime so not caching anywhere
-            axios.get('https://lolchess.gg/profile/na/' + currNames[i]).then(function(result)
+            axios.get("https://lolchess.gg/profile/na/" + currNames[i]).then(function(result)
             {
                 $ = cheerio.load(result.data)
-                userTier = $('span.profile__tier__summary__tier')
+                userTier = $("span.profile.tier__summary__tier")
                 userDiv = $(userTier).text().slice(-1)
                 userTier = $(userTier).text().slice(0 , -2)
-                userName = $('span.profile__summoner__name')
-                userLP = $('span.profile__tier__summary__lp')
+                userName = $("span.profile__summoner__name")
+                userLP = $("span.profile__tier__summary__lp")
                 output.push([$(userName).text() , userTier , parseInt(userDiv) , parseInt($(userLP).text().slice(0 , 2))])
                 count++
 
                 if (count == currNames.length)
-                    doneSort(output)
+                    message.reply("\nRanks successfully updated.")
             })
         }
+    }
+
+    if (message.content === "!ranks")
+    {
+        rankSortOutput(rankCache)
     }
 }) ;
 
